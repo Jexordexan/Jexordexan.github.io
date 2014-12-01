91b42190d67a1163b63b91785aa596af62638e6e
A Jordan Simonds <simona3@rpi.edu>
2014-12-01 11:52:49 -0500

    version 1.0

---
+++ index.php
@@ -0,0 +1,299 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>AJ Simonds</title>
+<link href="//netdna.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.css" rel="stylesheet">
+<link href="css/site2.css" rel="stylesheet">
+<link href="css/animate.css" rel="stylesheet">
+<link rel="shortcut icon" href="images/AJS-favicon.png" />
+
+<script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+<script src="js/fadenscroll.js"></script>
+<script src="js/wow.min.js"></script>
+<script>
+ new WOW().init();
+</script>
+<meta name="viewport" content="width=device-width" /> 
+</head>
+
+<body>
+<div id="curtain"></div>
+<div class="tease"></div>
+<div class="side">
+
+	<img src="images/AJS-Designer-07.png" width="200">
+  <h2><strong>Site Menu</strong></h2>
+  <hr>
+  <nav>
+    <a data-href="#header"><li><i class="fa fa-home fa-fw"></i> Home </li></a>
+    <a data-href="#design"><li><i class="fa fa-th-large fa-fw"></i> Design </li></a>
+    <a data-href="#skills"><li><i class="fa fa-magic fa-fw"></i> Skills </li></a>
+    <a data-href="#about"><li><i class="fa fa-info-circle fa-fw"></i> About </li></a>
+    <a data-href="#footer"><li><i class="fa fa-envelope fa-fw"></i> Contact </li></a>
+    
+    <a href="https://www.facebook.com/JrummrBoi"><li id="fb"><i class="fa fa-facebook-square fa-fw"></i> Facebook </li></a>
+    <a href="http://codepen.io/jexordexan"><li id="cp"><i class="fa fa-codepen fa-fw"></i> CodePen </li></a>
+    <a href="http://be.net/curvlinear"><li id="be"><i class="fa fa-behance-square fa-fw"></i> B&eacute;hance </li></a>
+    <a href="https://my.indeed.com/me/Jordan_Simonds"><li id="rs"><i class="fa fa-file-text fa-fw"></i> Resume </li></a>
+
+    
+    
+    
+  </nav>
+  <div class="grab"><i class="fa fa-bars fa-lg"></i></div>
+</div>
+
+<div id="spacer">
+</div>
+<div id="container">
+  
+
+<div id="header" class="fore header">
+<div  id="logo-div" >
+    <a href="#">
+    <svg version="1.1" id="logo" x="0px" y="0px" width="300px" height="300px"
+   viewBox="0 0 180 175" enable-background="new 0 0 180 180" xml:space="preserve">
+
+  <g>
+    <g>
+      <circle id="circle" cx="90" cy="90.001" r="75.343"/> 
+    </g>
+    <g id="shadow">
+      <path d="M165.343,90.001c0-0.362-0.022-0.719-0.027-1.079l-34.373-34.374c0,36.194,0,11.124,0,46.791
+        c0,14.037-14.037,28.077-28.076,28.077c-18.716,0-18.716,0-18.716,0l-14.038-21.06H56.077h-7.019l55.557,55.556
+        C139.228,157.106,165.343,126.607,165.343,90.001z"/>
+      <path d="M84.151,73.265h28.076c0,0,0,23.395,0,28.074"/>
+    </g>
+    <g>
+      <path id="AJ" d="M84.151,54.548v18.717h28.076c0,0,0,23.395,0,28.074c0,4.681-4.68,9.357-9.359,9.357
+        c-4.679,0-9.359,0-9.359,0L56.077,54.548h-7.019v21.057l7.019,9.359l4.68,7.018h-4.68h-7.019v16.375h7.019h14.037l14.038,21.06
+        c0,0,0,0,18.716,0c14.039,0,28.076-14.04,28.076-28.077c0-35.667,0-10.597,0-46.791H84.151z"/>
+    </g>
+  </g>
+    <linearGradient id="grad3" x1="0%" y1="0%" x2="100%" y2="100%">
+      <stop offset="0%" style="stop-color:#B36000;stop-opacity:1" />
+      <stop offset="100%" style="stop-color:#B36000;stop-opacity:.25" />
+    </linearGradient>
+</svg>
+    </a>
+    <style>
+    #logo-div{
+  -webkit-animation:rotate-in 1s ease;
+}
+
+
+#logo:hover{
+  -webkit-transform-origin:50% 50%; 
+  -webkit-animation:scale .5s ease;
+}
+
+
+#circle {
+  fill: #FF8B00;
+}
+
+#shadow {
+  fill: url(#grad3);
+  -webkit-animation:fade-in 3s ease;
+}
+
+#AJ {
+  fill:#FFFFFF;
+}
+
+circle, #AJ{
+  stroke: #999;
+  stroke-width:0; 
+  stroke-dasharray: 600;
+  stroke-linecap: round;
+  stroke-linejoin: square;
+  -webkit-animation: grow 2s ease-in;
+}
+
+@-webkit-keyframes grow {
+  0%{
+    stroke-dashoffset: 600;
+     fill-opacity:0; 
+     stroke-width:1;
+  }
+  
+  50%{
+    stroke: #FFF; 
+    stroke-dashoffset: 0;
+      fill-opacity:0; 
+      stroke-width:1.5; 
+  }
+  
+  100%{
+    stroke-dashoffset: 0;
+        stroke-width:0;
+       fill-opacity:1;
+  }
+}
+
+@-webkit-keyframes fade-in {
+  0%{fill-opacity:0;}
+  50%{fill-opacity:0;}
+  100%{fill-opacity:1;}
+}
+
+@-webkit-keyframes rotate-in {
+  from{-webkit-transform: scale(2) rotate(-90deg)}
+  to{-webkit-transform: scale(1) rotate(0deg)}
+}
+
+@-webkit-keyframes scale {
+  from{-webkit-transform: scale(1.3)}
+  to{-webkit-transform: scale(1))}
+}
+</style>
+</div>
+<!-- <img id="banner" src="images/AJS-Design-05.png" alt="AJS Design"> -->
+  <p>The odds are I haven't met you yet. This is unfortunate. What I can do is help you learn more about me!</p>
+  <p>I am an experienced and creative designer with an array of specialized skillsets. Keep reading!</p>
+    <hr>
+    <div class="fourth"><a data-href="#design"><li><i class="fa fa-th-large fa-fw"></i> My Design </li></a></div>
+    <div class="fourth"><a data-href="#skills"><li><i class="fa fa-magic fa-fw"></i> Skills </li></a></div>
+    <div class="fourth"><a data-href="#about"><li><i class="fa fa-info-circle fa-fw"></i> About Me </li></a></div>
+    <div class="fourth"><a data-href="#footer"><li><i class="fa fa-envelope fa-fw"></i> Contact Me </li></a></div>
+    
+</div>
+
+<div id="design" class="back"></div>
+<div class="fore">
+  <h1><i class="fa fa-th"></i> My Design</h1>
+  <hr><div class="fourth skill-pane">
+  <img class="skill wow zoomIn" src="images/skillBox-10.png"  alt="Web Design">
+  <h2>Get the fresh look your company deserves</h2>
+  <p>I design on the cusp of modern aesthetic and work closely with the client to acheive the best user satisfaction possible. Beauty and function are incoorporated with equal care.</p><hr>
+  <!-- <div class="examples">
+    <div class="example"><img src="images/projects/outing-club-site.PNG"><figcaption>Outing Club</figcaption></div>
+    <div class="example"><img src="images/projects/joy-site.PNG"><figcaption>Olivia's Joy</figcaption></div>
+  </div>
+  <a href="#"><div class="seeMore">See More...</div></a> -->
+  </div><div class="fourth skill-pane">
+  <img class="skill wow zoomIn" src="images/skillBox-11.png"  alt="Branding">
+  <h2>Your corporate identity is my top priority</h2>
+  <p>I can help you update your company's image with a fresh logo designed to match your vision. My logos are scalable, and guarunteed for use across all media.</p><hr>
+  </div><div class="fourth skill-pane">
+  <img class="skill wow zoomIn" src="images/skillBox-12.png"  alt="Poster">
+  <h2>I'll make sure people notice your organization or event</h2>
+  <p>Posters are a very effective way of increasing awareness and attendance. I design them with savvy typography and layout that will be sure to put the important information first.</p><hr>
+  </div><div class="fourth skill-pane">
+  <img class="skill wow zoomIn" src="images/skillBox_Artboard 15.png"  alt="infographic">
+  <h2>Statistics and information can look really good!</h2>
+  <p>Data is all around us and sometimes we dont understand it all. Infographics can visualize data for you and help educate your audience. They work well for web and print.</p><hr>
+  </div>
+</div>
+
+<div class="back" id="skills"></div>
+<div class="fore" id="skills-block">
+  <h1><i class="fa fa-magic fa-fw"></i> Skills</h1>
+  <hr>
+  <div id="expertise" class="list">
+    <h2>Expertise</h2>
+  <ul>
+        <li class="wow rollIn" data-wow-delay="0.0s"><img class="icon" src="/images/skills/skill-02.png"><span class="iconTitle">Photoshop</span></li>
+        <li class="wow rollIn" data-wow-delay="0.1s"><img class="icon" src="/images/skills/skill-03.png"><span class="iconTitle">Illustrator</span></li>
+        <li class="wow rollIn" data-wow-delay="0.2s"><img class="icon" src="/images/skills/skill-04.png"><span class="iconTitle">Dreamweaver</span></li>
+        <li class="wow rollIn" data-wow-delay="0.3s"><img class="icon" src="/images/skills/skill-10.png"><span class="iconTitle">Drupal</span></li>
+        <li class="wow rollIn" data-wow-delay="0.4s"><img class="icon" src="/images/skills/skill-11.png"><span class="iconTitle">Github</span></li>
+        <li class="wow rollIn" data-wow-delay="0.5s"><img class="icon" src="/images/skills/skill-05.png"><span class="iconTitle">Blender 3D</span></li>
+        <li class="wow rollIn" data-wow-delay="0.6s"><img class="icon" src="/images/skills/skill-09.png"><span class="iconTitle">MS Office</span></li> 
+        <li class="wow rollIn" data-wow-delay="0.7s"><img class="icon" src="/images/skills/skill-06.png"><span class="iconTitle">Typography</span></li>
+        <li class="wow rollIn" data-wow-delay="0.8s"><img class="icon" src="/images/skills/skill-07.png"><span class="iconTitle">Color Theory</span></li>
+        <li class="wow rollIn" data-wow-delay="0.9s"><img class="icon" src="/images/skills/skill-08.png"><span class="iconTitle">Print</span></li>              
+    </ul>
+  </div><hr />
+  <div id="languages" class="list">
+    <h2>Languages</h2>
+  <ul>
+        <li><span class="langTitle" style="background:#D63; height:220px">HTML5</span></li>
+        <li><span class="langTitle" style="background:#29D; height:230px">CSS3</span></li>
+        <li><span class="langTitle" style="background:#5B5; height:160px">JavaScript</span></li>
+        <li><span class="langTitle" style="background:#26A; height:150px">JQuery</span></li>
+        <li><span class="langTitle" style="background:#929; height:100px">JSON/AJAX</span></li>
+        <li><span class="langTitle" style="background:#C44; height:140px">MySQL</span></li> 
+        <li><span class="langTitle" style="background:#93C; height:175px">PHP</span></li>
+        <li><span class="langTitle" style="background:#FC2; height:100px">Python</span></li>
+        <li><span class="langTitle" style="background:#3C9; height:90px">MATLAB</span></li>
+        <li><span class="langTitle" style="background:#AAA; height:30px">C</span></li>
+    </ul>
+  </div><hr />
+  
+  <div id="hobbies" class="list">
+    <h2>Hobbies</h2>
+    <ul>
+   <!-- <li><img class="icon" src=""><span class="iconTitle">Photography</span></li>
+        <li><img class="icon" src=""><span class="iconTitle">Hiking</span></li>
+        <li><img class="icon" src=""><span class="iconTitle">Cooking</span></li>
+        <li><img class="icon" src=""><span class="iconTitle">Doodling</span></li>
+        <li><img class="icon" src=""><span class="iconTitle">Designing</span></li>
+        <li><img class="icon" src=""><span class="iconTitle">Snowboarding</span></li>   -->
+        <li><span class="langTitle">Photography</span></li>
+        <li><span class="langTitle">Hiking</span></li>
+        <li><span class="langTitle">Cooking</span></li>
+        <li><span class="langTitle">Doodling</span></li>
+        <li><span class="langTitle">Designing</span></li>
+        <li><span class="langTitle">Snowboarding</span></li>        
+    </ul>
+  </div>
+</div>
+
+
+
+
+<div class="back">
+</div>
+  
+<div id="about" class="fore">
+  <h1><i class="fa fa-info-circle fa-fw"></i> About Me</h1>
+  <hr>
+
+    
+
+  <div id="bio">
+
+      <div class="center"><img id="portrait" src="images/me.jpg" alt="Me"></div>
+    <h2>Education</h2><hr>
+    <p><strong>Rensselaer Polytechnic Institute</strong> <br>
+    BS Mechanical Engineering, Class of 2015
+    </p>
+    <br>
+    <h2>Experience</h2><hr>
+    <p><strong>Rensselaer Union</strong><br>Lead Web Designer</p>
+    <p><strong>Rensselaer Outing Club</strong><br>Lead Web Developer/Designer, Webmaster</p>
+    <p><strong>Lifenets Foundation</strong><br>Webmaster</p>
+    <br>
+    <h2>Bio</h2><hr>
+    <p>Hi, my name is Arthur Jordan Simonds. You can call me Jordan! I spent my entire childhood in Newtown, Connecticut, and now I live in Troy, New York studying to become a Mechanical Engineer at Rensselaer Polytechnic Institute (better known as RPI). I am a full-time student and an enthusiastic part-time graphic designer. I have a keen sense of style and can make pretty much anything look good. In fact, that is my motto: "Look good!"</p>
+    <p>Anyway, I started designing in 3D back in high school using a free program called Blender. I then progressed to print design and made dozens of unique posters for various events, clubs and staff departments around campus here at good ol' RPI. I worked mainly in Adobe Illustrator, with some dabbling in Photoshop and InDesign.</p>
+    <p>My passion for digital design grew and grew. Soon I was taking classes and starting my own projects. I really enjoyed designing posters but I wanted to make something more global and timeless. There is nothing better than a logo, I thought, and so I got into branding. I studied hundreds of logos, new and old, trying to appreciate the effort that went into each one. I analyzed their composition, their color, and most importantly how the logo matched the organization. I now have a solid history in branding, habing made logos for clubs, companies, video games, events, and even myself.</p>
+    <p>In the spring of 2013, another opportunity arose. The Rensselaer Outing Club, a club I am very active in on campus, lost all of their database information including members, leaders, equipment and officer positions. The old server with the website crashed and only a limited amount of information was recoverable. I promised to completely redesign the website and learn whatever languages and skills I had to in order to get it back to a fully functioning website and database. I taught myself several web languages and how to structure an efficient database.</p>
+    <p>By the end of the summer, I had created a beautiful website with all the functionality needed to restock our database of epuipment and members. It had many new features that the old site did not.
+    </p>
+  </div>
+</div>
+
+
+<div id="third" class="back">
+</div>
+
+<div id="footer" class="fore footer">
+  <h1><i class="fa fa-envelope fa-fw"></i> Contact</h1>
+    <hr>
+    <div>
+      <a href="mailto:simonds.jordan@gmail.com"><li id="be"><i class="fa fa-envelope fa-fw"></i> Email Me</a></li>
+      <a href="https://www.facebook.com/JrummrBoi"><li id="fb"><i class="fa fa-facebook-square fa-fw"></i> Facebook </li></a>
+      <a href="http://codepen.io/jexordexan"><li id="cp"><i class="fa fa-codepen fa-fw"></i> CodePen </li></a>
+      <a href="http://be.net/curvlinear"><li id="be"><i class="fa fa-behance-square fa-fw"></i> B&eacute;hance </li></a>
+      <a href="https://my.indeed.com/me/Jordan_Simonds"><li id="rs"><i class="fa fa-file-text fa-fw"></i> Resume </li></a>
+    </div>
+</div>
+  
+</div>
+
+
+</body>
+</html>
